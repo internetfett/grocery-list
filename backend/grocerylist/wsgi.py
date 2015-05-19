a04c587db350c12b32c32798992ea0ddfb72836a
Cody Kinsey <internetfett@gmail.com>
2015-05-19 15:42:45 -0500

    update paths

--- backend/grocerylist/wsgi.py
+++ backend/grocerylist/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "grocery-list.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "grocerylist.settings")
 
 application = get_wsgi_application()
