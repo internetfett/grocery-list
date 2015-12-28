da9f0b809296b952a0252b690fdfb3df7d89aa32
Cody Kinsey <internetfett@gmail.com>
2015-12-27 23:02:38 -0600

    fix deprecation warning

--- frontend/app/models/checklist.js
+++ frontend/app/models/checklist.js
@@ -2,8 +2,8 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
     name: DS.attr(),
-    checklist_ingredients: DS.hasMany('checklist_ingredient'),
-    checklist_items: DS.hasMany('checklist_item'),
+    checklist_ingredients: DS.hasMany('checklist_ingredient', {async: false}),
+    checklist_items: DS.hasMany('checklist_item', {async: false}),
 
     combined: function() {
         var ingredients = this.get('checklist_ingredients');
