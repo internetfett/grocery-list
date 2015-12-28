ff9503431454d99b03190ac24cfd2c746c0fa195
Cody Kinsey <internetfett@gmail.com>
2015-12-27 21:59:35 -0600

    placeholder code for injecting app context, ability to set page title

--- frontend/app/router.js
+++ frontend/app/router.js
@@ -2,7 +2,15 @@ import Ember from 'ember';
 import config from './config/environment';
 
 const Router = Ember.Router.extend({
-  location: config.locationType
+  location: config.locationType,
+  setTitle: function(title) {
+    var renderer = this.container.lookup('renderer:-dom');
+    if(renderer) {
+        Ember.set(renderer, '_dom.document.title', title);
+    } else {
+        document.title = title;
+    }
+  },
 });
 
 Router.map(function() {
@@ -11,3 +19,30 @@ Router.map(function() {
 });
 
 export default Router;
+
+Ember.Route.reopen({
+  init: function() {
+    if(this.title) {
+      this.router.setTitle(this.title);
+    }
+    this._super.apply(this, arguments);
+  },
+  /*setupController: function(controller, model) {
+    this._super(controller, model);
+
+    var route = this;
+    var appController = this.controllerFor('application');
+
+    if(this._appContextVars) {
+      this._appContextVars.forEach(function(item) {
+        appController.set(item, route.get(item));
+      });
+    }
+
+    if(this._appContextModelAttrs) {
+      this._appContextModelAttrs.forEach(function(item) {
+        appController.set(item, model.get(route.get(item)));
+      });
+    }
+  },*/
+});
--- frontend/app/routes/checklist.js
+++ frontend/app/routes/checklist.js
@@ -1,6 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+    //_appContextModelAttrs: ['pageHeader'],
+    //_appContextVars: ['pageFooter'],
+    //pageHeader: 'name',
+    //pageFooter: 'This is the footer content.',
+    title: 'Checklist',
     model(params) {
         return this.store.findRecord('checklist', params.id);
     },
