d0f99b30ade4d239dec4b162a413380c5252aa8b
Cody Kinsey <internetfett@gmail.com>
2015-12-31 16:03:35 -0600

    add ember modals

--- frontend/app/controllers/checklist.js
+++ frontend/app/controllers/checklist.js
@@ -3,4 +3,10 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
     sortedList: Ember.computed.sort('model.combined', '_combinedSort'),
     _combinedSort: ['status:asc', 'name:asc'],
+    isShowingModal: false,
+    actions: {
+        toggleModal: function() {
+            this.toggleProperty('isShowingModal');
+        }
+    },
 });
