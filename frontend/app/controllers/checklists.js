d4bf9f6cf206e08c9cd32e1abc40564b0572701c
Cody Kinsey <internetfett@gmail.com>
2016-01-15 13:56:37 -0600

    ability to add checklists

---
+++ frontend/app/controllers/checklists.js
@@ -0,0 +1,21 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+    isShowingModal: false,
+    actions: {
+        createChecklist: function() {
+            // TODO: Replace with Oauth2 User
+            var existing = this.model.get('firstObject');
+            var checklist = this.store.createRecord('checklist', {
+                'name': this.get('inputName'),
+                'user': existing.get('user'),
+            });
+            checklist.save().then(function(){}, function(){ alert('failure'); });
+            this.send('toggleModal');
+        },
+        toggleModal: function() {
+            this.toggleProperty('isShowingModal');
+        }
+    },
+    inputName: '',
+});
